64eeee196764f0d716acf8e204a3f3cdd1887a96
Erin Claudio <eclaudio@gmail.com>
2025-02-05 09:37:11 -0500

    Add ios and gitignore

---
+++ gameday-ios/gameday-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gameday-ios
+//
+//  Created by erin claudio on 2/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
