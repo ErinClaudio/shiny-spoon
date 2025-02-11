655b7129419ddd71bc2029c9e112049c095587a4
Erin Claudio <eclaudio@gmail.com>
2025-02-11 09:02:29 -0500

    Revert "Video 3 of the tutorial"

--- gameday-desktop/config/routes.rb
+++ gameday-desktop/config/routes.rb
@@ -14,10 +14,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "static#index"
-
-  namespace :turbo do
-    namespace :ios do
-      resource :path_configuration, only: [:show]
-    end
-  end
 end
